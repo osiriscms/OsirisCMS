6e3c7ee96d02ede09a7576956c21e7ad29e5d9a4
master <shumeevd@gmail.com>
2012-11-27 03:32:14 +0300

    build 1

---
+++ index.php
@@ -0,0 +1,17 @@
+<?
+/**
+ * OsirisCMS - Content Manager System.
+ * Index Page.
+ *
+ * @version OsirisCMS 1.0.1
+ * @author Shumeev Dmitry (shumeevd@gmail.com)
+ **/
+
+session_start(); 
+
+include_once('config.inc.php');
+
+include_once($generalConfig["system_files"].'/core/mysql.class.php');
+include_once($generalConfig["system_files"].'/core/core.class.php');
+
+?>
